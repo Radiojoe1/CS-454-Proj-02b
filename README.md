379c814315c6b740e15cd5fd3abb225a30d2d89a
wslaybau <wslaybau@ucsc.edu>
2026-03-29 15:20:20 -0700

    Extra Credit Implementation

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # CS-454-Proj-02b
 
-Members: 
+Members: William Dappen & Nick Guevera
+
+To Run:
+Compilation Command:
+    g++ main.cpp MultiplierArray.cpp -o CS_454_Proj_02b.exe
+
+Cmake file:
+    cmake_minimum_required(VERSION 4.0)
+    project(CS_454_Proj_02b)
+    
+    set(CMAKE_CXX_STANDARD 14)
+    
+    include_directories(.)
+    
+    add_executable(CS_454_Proj_02b
+    main.cpp
+    MultiplierArray.cpp
+    MultiplierArray.h)
+
+Extra Credit:
+    Compute the time taken for evaluating A^B using this O(n^2) algorithm for multiplication.
+    Compare it to the time taken using the O(n^1.585) karatsuba algorithm for multiplication.
+    Note number of digits in AB is n = log10(A^B) = B log10(A).
+
+        The time taken for evaluating A^B using a divide and conquer exponentiation algorithm
+        and a O(n^2) multiplication algorithm is equal to the product of the time complexity
+        required by the multiplication algorithm, and the time complexity of the exponentiation
+        algorithm. We already know the number of multiplications done by the exponentiation
+        algorithm to be log2(b), and we know that the time complexity of the gradeschool 
+        algorithm is theta(n^2). Therefore the time complexity of the exponentiation algorithm
+        is theta(n^2 * log2(b)), where n is the number of digits in A^B. Knowing that 
+        n = Blog10(A), the final complexity is theta((B log10(A))^2 * log2(b)). Using the
+        same process, the karatsuba-based exponentiation algorithm would come out to a
+        complexity of theta((B log10(A))^1.585 * log2(b)), making it far faster than the
+        gradeschool algorithm. This is true because the exponent in the n log n-like 
+        behavior of the algorithm dominates the behavior of the entire function.
+
+    Now fix the value of A (99 or 999) and generate several values of B 
+    (from 9 to 999 at an increment of 10) and plot the time taken to evaluate A^B as a function
+    of n using the two different methods of multiplication.
+
+    At what value of n is it evident that the karatsuba algorithm for multiplication
+    is superior to the O(n2) method of multiplication?
+
--- main.cpp
+++ main.cpp
@@ -22,7 +22,28 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <chrono>
+#include <cmath>
+#include <fstream>
 #include "MultiplierArray.h"
+
+using Clock = std::chrono::steady_clock;
+
+using Duration = std::chrono::duration<double, std::milli>;
+
+template <typename Func>
+Duration measureExecutionTime(Func func, const int& A, const int& B) {
+    auto start = Clock::now(); // Get the starting time point
+
+    // Call the function with its arguments
+    func(A, B);
+
+    auto end = Clock::now(); // Get the ending time point
+
+    // Calculate the duration and cast it to the desired unit (milliseconds in this case)
+    return std::chrono::duration_cast<Duration>(end - start);
+}
+
 
 int main(){
     int a;
@@ -52,6 +73,7 @@ int main(){
         std::cout << "1. Task 1: Multiply A * B" << std::endl;
         std::cout << "2. Task 2: Exponentiate A^B" << std::endl;
         std::cout << "3. Quit" << std::endl;
+        std::cout << "4. Extra credit calculation" << std::endl;
         std::cout << "Enter Choice: ";
 
 
@@ -69,14 +91,51 @@ int main(){
                 break;
             case 2:
                 result = MultiplierArray::exponentiation(a, b); // this is using the class overloaded * operator which implements the karatsuba algorithm
-                result2 = SchoolyardMultArray::exponentiation(a, b);
-                std::cout << "A ^ B = " << result.toString() << ", \n" << "A ^ B 2: " << result2.toString() << std::endl;
+                std::cout << "A ^ B = " << result.toString() << ", \n";
                 break;
-                case 3:
+            case 3:
                 std::cout << "Exiting program." << std::endl;
+                exit(1);
                 break;
+            case 4: {
+                std::cout << "Running Extra Credit Process. A is locked to 999, and B iterates from 9 to 999 on 10's\n";
+                std::ofstream outFile("output.csv");
+
+                if (outFile.is_open()) {
+                    // File opened successfully
+                } else {
+                    std::cerr << "Error opening file!" << std::endl;
+                    break;
+                }
+
+                std::vector<Duration> schoolTime, karatsubaTime;
+                std::vector<int> ArrayOfN;
+                int aInt = 999;
+                for(int bInt = 9; bInt<= 999; bInt+=10){
+                    schoolTime.push_back(measureExecutionTime(SchoolyardMultArray::exponentiation, aInt, bInt));
+                    karatsubaTime.push_back(measureExecutionTime(MultiplierArray::exponentiation, aInt, bInt));
+                    ArrayOfN.push_back(static_cast<int>(std::floor(bInt * std::log10(aInt))) + 1);
+                }
+
+                if(schoolTime.size() == karatsubaTime.size() && ArrayOfN.size() == schoolTime.size()){
+                    std::cout << "Success, writing to file\n";
+                    outFile << "n, school, karatsuba\n";
+                    for(size_t j = 0; j < schoolTime.size(); j++){
+                        outFile << ArrayOfN.at(j) << ','
+                                << schoolTime.at(j).count() << ','
+                                << karatsubaTime.at(j).count() << '\n';
+                    }
+                    outFile.close();
+                    std::cout << "write finished\n";
+                } else{
+                    std::cout << "Error writing\n";
+                }
+
+                break;
+            }
             default:
                 std::cout << "Invalid choice. Please try again." << std::endl;
+                break;
         }
     }
 
